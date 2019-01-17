0c881531d812e3a0991215d4ac2598363d06d38a
codeboy <861596052@qq.com>
2019-01-17 11:53:29 +0800

    Number1 change Test

--- Number1/app/src/main/java/com/example/administrator/servieceandbroadcast/MainActivity.java
+++ Number1/app/src/main/java/com/example/administrator/servieceandbroadcast/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.administrator.servieceandbroadcast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG="MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate: ");
     }
 }
